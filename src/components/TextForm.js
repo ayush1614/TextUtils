b0f3683d02f24f3496c22c9c5ab18306a300eae9
Ayush Katiyar <katiyarayush2000@gmail.com>
2022-01-20 15:13:51 +0530

    added the copy to clipboard functionality

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -17,11 +17,24 @@ export default function TextForm(props) {
         setText(newText);
     };
 
+    // removes extra spaces
     const handleExtraSpaces = () => {
         let newText = text.split(/[ ] + /);
         setText(newText.join(" "));
     }
 
+    const handleCopyText = () => {
+        let copyText = document.getElementById("textArea");
+
+        //select the text field
+        copyText.select();
+        copyText.setSelectionRange(0, 99999);  //for mobile devices
+
+        // copy the text inside the text field
+        navigator.clipboard.writeText(copyText.value);
+    }
+
+    //clears the text area 
     const handleClear = () => {
         let newText = '';
         setText(newText);
@@ -39,25 +52,26 @@ export default function TextForm(props) {
         <>
             <div className='container'>
                 {/* setting the props */}
-                <h1 style={{color: props.mode === 'light' ? 'black' : 'white'}}>{props.heading}</h1>
+                <h1 style={{ color: props.mode === 'light' ? 'black' : 'white' }}>{props.heading}</h1>
 
                 {/*onChange is used so that we can write in textarea . Helps in settting the state and updating it*/}
-                
+
                 <div className="form-group"><textarea className="form-control" value={text} onChange={handleChange} style={{ backgroundColor: props.mode === 'dark' ? 'grey' : 'white', color: props.mode === 'light' ? 'black' : 'white' }} id="textArea" rows="8" placeholder='text...'></textarea></div>
 
                 <button className="btn btn-primary my-2 mx-1" onClick={handleUpClick}>Change to UpperCase</button>
                 <button className="btn btn-primary my-1 mx-2" onClick={handleLoClick}>Change to LowerCase</button>
                 <button className="btn btn-primary my-1 mx-2" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
+                <button className="btn btn-primary my-1 mx-2" onClick={handleCopyText}>Copy text</button>
                 <button className="btn btn-primary my-1 mx-2" onClick={handleClear}>Clear</button>
 
             </div>
             <hr />
-            <div className="container" style={{color: props.mode === 'light' ? 'black' : 'white'}}>
+            <div className="container" style={{ color: props.mode === 'light' ? 'black' : 'white' }}>
                 <h4>Text Summary</h4>
                 <p>Words : {text.split(" ").length - 1} &nbsp; &nbsp; Characters  : {text.length}</p>
                 <p>{0.008 * (text.split(" ").length - 1)} Minutes to Read</p>
                 <h5>Preview</h5>
-                <p>{text.length >0 ? text : 'Enter something in textbox to preview'}</p>
+                <p>{text.length > 0 ? text : 'Enter something in textbox to preview'}</p>
             </div>
         </>
     )
