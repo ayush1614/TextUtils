b75660af799a4ff9bbd557d3be9764d310270eaa
Ayush Katiyar <katiyarayush2000@gmail.com>
2022-01-20 15:04:49 +0530

    added the dark mode functionality

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import './App.css';
+import { useState } from 'react';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 
 function App() {
+  const [mode, setMode] = useState('light');
+
+  const changeMode = () => {
+    if (mode === 'light') {
+      setMode('dark') ;
+      document.body.style.backgroundColor = '#042743' ;
+    }
+
+    else {
+      setMode('light');
+      document.body.style.backgroundColor = 'white' ;
+    }
+  }
   return (
     <>
-      <Navbar title="TextUtils" />
+      <Navbar title="TextUtils" mode={mode} changeMode={changeMode } />
       <div className="container my-3">
-        <TextForm heading="Enter text here"/>
+        <TextForm heading="Enter text here" mode={mode}/>
       </div>
     </>
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -18,45 +18,47 @@ export default function TextForm(props) {
     };
 
     const handleExtraSpaces = () => {
-        let newText =  text.split(/[ ] + /) ;
-        setText(newText.join(" ")) ; 
+        let newText = text.split(/[ ] + /);
+        setText(newText.join(" "));
     }
 
     const handleClear = () => {
         let newText = '';
-        setText(newText); 
+        setText(newText);
     };
 
-        const handleChange = (event) => {
-            setText(event.target.value);        //on typing we set the updated value  | helps to type in text area
-        };
+    const handleChange = (event) => {
+        setText(event.target.value);        //on typing we set the updated value  | helps to type in text area
+    };
 
 
-        let [text, setText] = useState('');             // useState hook
-        // setText is used to update the value of text variable
+    let [text, setText] = useState('');             // useState hook
+    // setText is used to update the value of text variable
 
-        return (
-            <>
-                <div className='container'>
-                    {/* setting the props */}
-                    <h1>{props.heading}</h1>
+    return (
+        <>
+            <div className='container'>
+                {/* setting the props */}
+                <h1 style={{color: props.mode === 'light' ? 'black' : 'white'}}>{props.heading}</h1>
 
-                    {/*onChange is used so that we can write in textarea . Helps in settting the state and updating it*/}
-                    <div className="form-group"><textarea className="form-control" value={text} onChange={handleChange} id="textArea" rows="8" placeholder='text...'></textarea></div>
-                    <button className="btn btn-primary my-2 mx-1" onClick={handleUpClick}>Change to UpperCase</button>
-                    <button className="btn btn-primary my-1 mx-2" onClick={handleLoClick}>Change to LowerCase</button>
-                    <button className="btn btn-primary my-1 mx-2" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
-                    <button className="btn btn-primary my-1 mx-2" onClick={handleClear}>Clear</button>
+                {/*onChange is used so that we can write in textarea . Helps in settting the state and updating it*/}
+                
+                <div className="form-group"><textarea className="form-control" value={text} onChange={handleChange} style={{ backgroundColor: props.mode === 'dark' ? 'grey' : 'white', color: props.mode === 'light' ? 'black' : 'white' }} id="textArea" rows="8" placeholder='text...'></textarea></div>
 
-                </div>
-                <hr />
-                <div className="container">
-                    <h4>Text Summary</h4>
-                    <p>Words : {text.split(" ").length - 1} &nbsp; &nbsp; Characters  : {text.length}</p>
-                    <p>{0.008 * (text.split(" ").length - 1)} Minutes to Read</p>
-                    <h5>Preview</h5>
-                    <p>{text}</p>
-                </div>
-            </>
-        )
-    }
+                <button className="btn btn-primary my-2 mx-1" onClick={handleUpClick}>Change to UpperCase</button>
+                <button className="btn btn-primary my-1 mx-2" onClick={handleLoClick}>Change to LowerCase</button>
+                <button className="btn btn-primary my-1 mx-2" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
+                <button className="btn btn-primary my-1 mx-2" onClick={handleClear}>Clear</button>
+
+            </div>
+            <hr />
+            <div className="container" style={{color: props.mode === 'light' ? 'black' : 'white'}}>
+                <h4>Text Summary</h4>
+                <p>Words : {text.split(" ").length - 1} &nbsp; &nbsp; Characters  : {text.length}</p>
+                <p>{0.008 * (text.split(" ").length - 1)} Minutes to Read</p>
+                <h5>Preview</h5>
+                <p>{text.length >0 ? text : 'Enter something in textbox to preview'}</p>
+            </div>
+        </>
+    )
+}
