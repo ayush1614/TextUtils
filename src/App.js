462b2f0445b3992bf08dc240ade75aca9bddffe2
Ayush Katiyar <katiyarayush2000@gmail.com>
2022-02-06 02:43:44 +0530

    minor fixes and routing setup

--- src/App.js
+++ src/App.js
@@ -3,39 +3,55 @@ import { useState } from 'react';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import Alert from './components/Alert';
+import About from './components/About'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState('light');
   const [alert, setAlert] = useState(null);
 
-  const showAlert = (message) =>{
-    setAlert(message) ; 
+  const showAlert = (message) => {
+    setAlert(message);
 
-    setTimeout(()=>{
-      setAlert(null) 
-    }, 2000) ; 
+    setTimeout(() => {
+      setAlert(null)
+    }, 2000);
   }
 
   const changeMode = () => {
     if (mode === 'light') {
-      setMode('dark') ;
-      document.body.style.backgroundColor = '#042743' ;
-      showAlert(' Dark mode enabled ') ; 
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
+      showAlert(' Dark mode enabled ');
     }
 
     else {
       setMode('light');
-      document.body.style.backgroundColor = 'white' ;
-      showAlert(' Light mode enabled ') ;
+      document.body.style.backgroundColor = 'white';
+      showAlert(' Light mode enabled ');
     }
   }
   return (
     <>
-      <Navbar title="TextUtils" mode={mode} changeMode={changeMode} />
-      <Alert alert={alert} />
-      <div className="container my-3">
-        <TextForm showAlert = {showAlert} heading="Enter text here" mode={mode}/>
-      </div>
+      <Router>
+        <Navbar title="TextUtils" mode={mode} changeMode={changeMode} />
+        <Alert alert={alert} />
+        <div className="container my-3">
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/">
+          <TextForm showAlert={showAlert} heading="Enter text here" mode={mode} />
+          </Route>
+        </Switch>
+        </div>
+      </Router>
     </>
   );
 }
