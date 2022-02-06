b8a7b02a8575c6871452cdc7af2930b23ee0b6fc
Ayush Katiyar <katiyarayush2000@gmail.com>
2022-02-06 13:37:42 +0530

    counting  and empty textarea functionality fixed

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
 
-
 export default function TextForm(props) {
 
     //change to uppercase 
@@ -24,16 +23,12 @@ export default function TextForm(props) {
         props.showAlert('Extra Spaces Removed')
     }
 
-    const handleCopyText = () => {
+    const CopyText = () => {
         let copyText = document.getElementById("textArea");
-
-        //select the text field
-        copyText.select();
-        copyText.setSelectionRange(0, 99999);  //for mobile devices
 
         // copy the text inside the text field
         navigator.clipboard.writeText(copyText.value);
-
+        document.getSelection().removeAllRanges() ;
         props.showAlert('Copied to Clipboard')
     }
 
@@ -56,26 +51,26 @@ export default function TextForm(props) {
         <>
             <div className='container'>
                 {/* setting the props */}
-                <h1 style={{ color: props.mode === 'light' ? 'black' : 'white' }}>{props.heading}</h1>
+                <h1 className='mb-4' style={{ color: props.mode === 'light' ? 'black' : 'white' }}>{props.heading}</h1>
 
                 {/*onChange is used so that we can write in textarea . Helps in settting the state and updating it*/}
 
-                <div className="form-group"><textarea className="form-control" value={text} onChange={handleChange} style={{ backgroundColor: props.mode === 'dark' ? 'grey' : 'white', color: props.mode === 'light' ? 'black' : 'white' }} id="textArea" rows="8" placeholder='text...'></textarea></div>
+                <div className="form-group"><textarea className="form-control" value={text} onChange={handleChange} style={{ backgroundColor: props.mode === 'dark' ? '#3f5c74' : 'white', color: props.mode === 'light' ? 'black' : 'white' }} id="textArea" rows="8"></textarea></div>
 
-                <button className="btn btn-primary my-2 mx-1" onClick={handleUpClick}>Change to UpperCase</button>
-                <button className="btn btn-primary my-1 mx-2" onClick={handleLoClick}>Change to LowerCase</button>
-                <button className="btn btn-primary my-1 mx-2" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
-                <button className="btn btn-primary my-1 mx-2" onClick={handleCopyText}>Copy text</button>
-                <button className="btn btn-primary my-1 mx-2" onClick={handleClear}>Clear</button>
+                <button disabled={text.length===0} className="btn btn-primary my-2 mx-1" onClick={handleUpClick}>Change to UpperCase</button>
+                <button disabled={text.length===0} className="btn btn-primary my-1 mx-2" onClick={handleLoClick}>Change to LowerCase</button>
+                <button disabled={text.length===0} className="btn btn-primary my-1 mx-2" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
+                <button disabled={text.length===0} className="btn btn-primary my-1 mx-2" onClick={CopyText}>Copy text</button>
+                <button disabled={text.length===0} className="btn btn-primary my-1 mx-2" onClick={handleClear}>Clear</button>
 
             </div>
             <hr />
             <div className="container" style={{ color: props.mode === 'light' ? 'black' : 'white' }}>
                 <h4>Text Summary</h4>
-                <p>Words : {text.split(" ").filter((element)=>{return element.length>0}).length} &nbsp; &nbsp; Characters  : {text.length}</p>
+                <p>Words : {text.split(/\s+/).filter((element)=>{return element.length>0}).length} &nbsp; &nbsp; Characters  : {text.length}</p>
                 <p>{0.008 * (text.split("").filter((element)=>{return element!==' '}).length)} Minutes to Read</p>
                 <h5>Preview</h5>
-                <p>{text.length > 0 ? text : 'Enter something in textbox to preview'}</p>
+                <p>{text.length > 0 ? text : 'Nothing to preview'}</p>
             </div>
         </>
     )
