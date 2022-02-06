ec68676f35f57307036f66bef696c2883ed1812d
Ayush Katiyar <katiyarayush2000@gmail.com>
2022-02-06 13:34:12 +0530

    improved the alert functionaliy

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -3,9 +3,11 @@ import React from 'react';
 export default function Alert(props) {
 
     return (
-        props.alert && <div className={`alert alert-success alert-dismissible fade show `} role="alert">
-            {props.alert}
-            <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+
+        <div className='container' style={{ height: '50px' }}>
+            {props.alert && <div className={`alert alert-success alert-dismissible fade show `} role="alert">
+                {props.alert}
+            </div>}
         </div>
     )
 }
